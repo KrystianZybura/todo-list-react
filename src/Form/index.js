3de929991194ebed94e0a1c3684ff0caefb96d11
Krystian Żybura <Krystian12324444@gmail.com>
2023-04-01 20:44:54 +0200

    refactoring

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,17 +1,17 @@
 import "./style.css"
 
 const Form = () => (
-    <form className="form">
-            <input
-              className="form__inputText"
-              placeholder="Co jest do zrobienia?"
-            />
-            <input
-              className="form__button"
-              type="submit"
-              value="Dodaj zadanie"
-            />
-          </form>
+  <form className="form">
+    <input
+      className="form__inputText"
+      placeholder="Co jest do zrobienia?"
+    />
+    <input
+      className="form__button"
+      type="submit"
+      value="Dodaj zadanie"
+    />
+  </form>
 );
 
 export default Form;
