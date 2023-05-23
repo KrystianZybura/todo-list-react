d2c8abc473d7daa9f5495367ffe152d1f83cb291
Krystian Żybura <Krystian12324444@gmail.com>
2023-05-23 22:35:22 +0200

    minor refactoring

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,24 +1,23 @@
 import "./style.css";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-    tasks.length > 0 && (
-        <div className="buttons">
-            <button
-                className="buttons__button"
-                onClick={toggleHideDone}
-                disabled={tasks.every(({ done }) => !done)}
-            >
-                {hideDone ? "Pokaż" : "Ukryj"} ukończone
-            </button>
-            <button
-                className="buttons__button"
-                disabled={tasks.every(({ done }) => done)}
-                onClick={setAllDone}
-            >
-                Ukończ wszystkie
-            </button>
-        </div>
-    )
-);
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) =>
+  tasks.length > 0 && (
+    <div className="buttons">
+      <button
+        className="buttons__button"
+        onClick={toggleHideDone}
+        disabled={tasks.every(({ done }) => !done)}
+      >
+        {hideDone ? "Pokaż" : "Ukryj"} ukończone
+      </button>
+      <button
+        className="buttons__button"
+        disabled={tasks.every(({ done }) => done)}
+        onClick={setAllDone}
+      >
+        Ukończ wszystkie
+      </button>
+    </div>
+  );
 
 export default Buttons;
