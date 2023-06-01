daa145cacdd807674fc574e1dde54d42b80508f7
Krystian Żybura <Krystian12324444@gmail.com>
2023-06-01 17:16:40 +0200

    adding GlobalStyles, removing old ones

---
+++ src/GlobalStyles.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  ::after,
+  ::before {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: "Montserrat", sans-serif;
+    background-color: #e7e7e7;
+  }
+`;
+
+export default GlobalStyles;
