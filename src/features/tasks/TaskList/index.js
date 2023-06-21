351cb3c76fb3fe03b03c3b11dfd69ec029c34a84
Krystian Żybura <Krystian12324444@gmail.com>
2023-06-21 11:50:14 +0200

    toggleTaskDone reducer implemented

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,17 +1,25 @@
 import { List, Item, Content, Button, ToggleDoneButton } from "./styled";
+import { toggleTaskDone } from "../tasksSlice";
+import { useDispatch } from "react-redux";
 
-const TaskList = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
-  <List>
-    {tasks.map((task) => (
-      <Item hidden={task.done && hideDone} done={task.done} key={task.id}>
-        <ToggleDoneButton onClick={() => toggleTaskDone(task.id)}>
-          {task.done ? "✓" : ""}
-        </ToggleDoneButton>
-        <Content done={task.done}>{task.content}</Content>
-        <Button onClick={() => removeTask(task.id)}>🗑</Button>
-      </Item>
-    ))}
-  </List>
-);
+const TaskList = ({ tasks, hideDone, removeTask }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <List>
+      {tasks.map((task) => (
+        <Item hidden={task.done && hideDone} done={task.done} key={task.id}>
+          <ToggleDoneButton
+            onClick={() => dispatch(toggleTaskDone({ id: task.id }))}
+          >
+            {task.done ? "✓" : ""}
+          </ToggleDoneButton>
+          <Content done={task.done}>{task.content}</Content>
+          <Button onClick={() => removeTask(task.id)}>🗑</Button>
+        </Item>
+      ))}
+    </List>
+  );
+};
 
 export default TaskList;
--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -9,9 +9,14 @@ const tasksSlice = createSlice({
     addTask: ({ tasks }, { payload }) => {
       tasks.push(payload);
     },
+    toggleTaskDone: ({ tasks }, { payload }) => {
+      const index = tasks.findIndex((task) => task.id === payload.id);
+
+      tasks[index].done = !tasks[index].done;
+    },
   },
 });
 
-export const { addTask } = tasksSlice.actions;
-export const { selectTasks } = (state) => state.tasks;
+export const { addTask, toggleTaskDone } = tasksSlice.actions;
+export const selectTasks = (state) => state.tasks;
 export default tasksSlice.reducer;
