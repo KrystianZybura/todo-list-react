e7e786114b1a3c352c3d3a056681e2525720909c
Krystian Żybura <Krystian12324444@gmail.com>
2023-06-23 15:39:38 +0200

    unnecessary Main component removed

--- src/common/Main/index.js
+++
@@ -1,5 +0,0 @@
-import { StyledMain } from "./styled";
-
-const Main = ({ children }) => <StyledMain>{children}</StyledMain>;
-
-export default Main;
--- src/common/Main/styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const StyledMain = styled.main`
-  max-width: 1000px;
-  margin: 0 auto;
-  padding: 0 20px;
-`;
-
-export { StyledMain };
