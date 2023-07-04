bde937eeb0b3639921e2c60f43bd25ed9a9b39c9
Krystian Żybura <Krystian12324444@gmail.com>
2023-07-04 10:39:32 +0200

    rootSaga added

---
+++ src/rootSaga.js
@@ -0,0 +1,8 @@
+import { all } from "redux-saga/effects";
+import { tasksSaga } from "./features/tasks/tasksSaga";
+
+function* rootSaga() {
+  yield all([tasksSaga()]);
+}
+
+export default rootSaga;
