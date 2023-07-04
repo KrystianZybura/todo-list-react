65a98aede970ee612e8c2a939a16602f662cb34b
Krystian Żybura <Krystian12324444@gmail.com>
2023-07-04 11:47:24 +0200

    changing author content (test)

--- src/features/author/index.js
+++ src/features/author/index.js
@@ -1,12 +1,20 @@
+import Section from "../../common/Section";
+
 const Author = () => (
-  <div>
-    <p>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
-      assumenda facilis illo quia sunt quis at, eum aliquam, consequuntur
-      excepturi aperiam. Expedita quam odit eum assumenda repudiandae porro
-      impedit sint?
-    </p>
-  </div>
+  <>
+    <Section
+      title={"O autorze"}
+      body={
+        <p>
+          {" "}
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+          assumenda facilis illo quia sunt quis at, eum aliquam, consequuntur
+          excepturi aperiam. Expedita quam odit eum assumenda repudiandae porro
+          impedit sint?{" "}
+        </p>
+      }
+    />
+  </>
 );
 
 export default Author;
