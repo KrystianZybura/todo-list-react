1270e555369078edcaaad0067d78eb1d2390f626
Krystian Żybura <Krystian12324444@gmail.com>
2023-07-04 11:37:10 +0200

    BrowserRouter and Author added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import {
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+} from "react-router-dom/cjs/react-router-dom.min";
 import Tasks from "./features/tasks/Tasks";
+import Author from "./features/author";
 
-const App = () => <Tasks />;
+const App = () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/todo-list-react">Strona główna</Link>
+        </li>
+        <li>
+          <Link to="/author">O autorze</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <Switch>
+      <Route path="/author">
+        <Author />
+      </Route>
+      <Route path="/todo-list-react">
+        <Tasks />
+      </Route>
+    </Switch>
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/features/author/index.js
@@ -0,0 +1,12 @@
+const Author = () => (
+  <div>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus
+      assumenda facilis illo quia sunt quis at, eum aliquam, consequuntur
+      excepturi aperiam. Expedita quam odit eum assumenda repudiandae porro
+      impedit sint?
+    </p>
+  </div>
+);
+
+export default Author;
