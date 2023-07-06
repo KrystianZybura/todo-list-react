1af51cdfe967b278f405f6390812e3109174ae0d
Krystian Żybura <Krystian12324444@gmail.com>
2023-07-06 20:25:49 +0200

    Task searcher implemented

--- src/features/tasks/TasksPage/TaskSearch/index.js
+++ src/features/tasks/TasksPage/TaskSearch/index.js
@@ -1,5 +1,30 @@
+import {
+  useHistory,
+  useLocation,
+} from "react-router-dom/cjs/react-router-dom.min";
 import Input from "../../Input/styled";
 
-const TaskSearch = () => <Input placeholder="Filtruj zadania" />;
+const TaskSearch = () => {
+  const location = useLocation();
+  const history = useHistory();
+  const query = new URLSearchParams(location.search).get("szukaj");
 
+  const onInputChange = ({ target }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    !target.value.trim()
+      ? searchParams.delete("szukaj")
+      : searchParams.set("szukaj", target.value);
+
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  };
+
+  return (
+    <Input
+      placeholder="Filtruj zadania"
+      value={query ?? ""}
+      onChange={onInputChange}
+    />
+  );
+};
 export default TaskSearch;
