644df81c5962d5304ded4ce8e1bc25c995316756
Krystian Żybura <121553175+KrystianZybura@users.noreply.github.com>
2023-07-11 14:02:34 +0200

    JSON source assigned to separate const

--- src/features/tasks/getExampleTasks.js
+++ src/features/tasks/getExampleTasks.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const EXAMPLE_TASKS_JSON = "/todo-list-react/exampleTasks.json";
+
 const getExampleTasks = async () => {
-  const response = await axios.get("/todo-list-react/exampleTasks.json");
+  const response = await axios.get(EXAMPLE_TASKS_JSON);
 
   return await response.data;
 };
